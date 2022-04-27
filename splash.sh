45706fc2d6a743fbb9eb411ba77b7f7bbbc6a6e4
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-04-27 18:04:20 +0200

    Adds single job for creating the splash image for all depending jobs

--- splash.sh
+++ splash.sh
@@ -2,9 +2,13 @@
 
 set -e
 
-VERSION=$1
-convert ./resources/emoflon-splash_template.png -font Liberation-Sans -pointsize 24 -draw "gravity south fill white text 0,15 '${VERSION}'" splash.bmp
-mkdir -p ./eclipse/plugins/org.emoflon.splash
-mv splash.bmp ./eclipse/plugins/org.emoflon.splash
-sed -i 's/org.eclipse.epp.package.common/org.emoflon.splash/g' ./eclipse/eclipse.ini
-sed -i 's/osgi.splashPath=platform\\:\/base\/plugins\/org.eclipse.platform/osgi.splashPath=platform\\:\/base\/plugins\/org.emoflon.splash/g' ./eclipse/configuration/config.ini
+MODE=$1
+VERSION=$2
+if [[ "$MODE" = "img" ]]; then
+    convert ./resources/emoflon-splash_template.png -font Liberation-Sans -pointsize 24 -draw "gravity south fill white text 0,15 '${VERSION}'" splash.bmp
+elif [[ "$MODE" = "deploy" ]]; then
+    mkdir -p ./eclipse/plugins/org.emoflon.splash
+    mv splash.bmp ./eclipse/plugins/org.emoflon.splash
+    sed -i 's/org.eclipse.epp.package.common/org.emoflon.splash/g' ./eclipse/eclipse.ini
+    sed -i 's/osgi.splashPath=platform\\:\/base\/plugins\/org.eclipse.platform/osgi.splashPath=platform\\:\/base\/plugins\/org.emoflon.splash/g' ./eclipse/configuration/config.ini
+fi
