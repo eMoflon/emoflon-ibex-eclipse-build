54bfeb343d1cfb1f4d35a8445d27b6b20c2ab550
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-05-03 14:23:15 +0200

    Adds check for existing splash.bmp file to splash script

--- splash.sh
+++ splash.sh
@@ -8,6 +8,12 @@ if [[ "$MODE" = "img" ]]; then
     convert ./resources/emoflon-splash_template.png -font Liberation-Sans -pointsize 24 -draw "gravity south fill white text 0,15 '${VERSION}'" splash.bmp
 elif [[ "$MODE" = "deploy" ]]; then
     mkdir -p ./eclipse/plugins/org.emoflon.splash
+
+    # check if splash.bmp exists
+    if [[ ! -f splash.bmp ]]; then
+        echo "=> splash.bmp not found. Exit."; exit 1 ;
+    fi
+
     mv splash.bmp ./eclipse/plugins/org.emoflon.splash
     sed -i 's/org.eclipse.epp.package.common/org.emoflon.splash/g' ./eclipse/eclipse.ini
     sed -i 's/osgi.splashPath=platform\\:\/base\/plugins\/org.eclipse.platform/osgi.splashPath=platform\\:\/base\/plugins\/org.emoflon.splash/g' ./eclipse/configuration/config.ini
